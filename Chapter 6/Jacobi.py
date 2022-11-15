93ed704ce06eda4ecd6548cb21166db0fb0e23b3
astridmo <astrid.moum@gmail.com>
2022-11-15 20:36:44 +0100

    cleanup

--- Chapter 6/Jacobi.py
+++ Chapter 6/Jacobi.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-M = np.array([[0, -0.25, -1/8], [-2/3, 0, -1/3], [-3/14, -2/7, 0]])
+M = np.array([[0, -0.25, -1 / 8], [-2 / 3, 0, -1 / 3], [-3 / 14, -2 / 7, 0]])
 A = np.array([[8, 2, 1], [4, 6, 2], [3, 4, 14]])
 y = np.array([3, 4, 2])
 
@@ -13,7 +13,7 @@ Dinv = np.linalg.inv(D)  # Inverse of matrix D
 # Start value of x
 x = np.array([0, 0, 0])
 
-epsilon = 10**-4
+epsilon = 10 ** -4
 
 print(M)
 
@@ -26,7 +26,7 @@ A = np.array([[8, 2, 1], [4, 6, 2], [3, 4, 14]])
 x = np.array([0, 0, 0])
 
 
-def Jacobi(A, x, y, epsilon):
+def jacobi(A, x, y, epsilon):
     D = np.diag(np.diag(A))  # Diagonal matrix
     Dinv = np.linalg.inv(D)  # Inverse of matrix D
     M = np.matmul(Dinv, (D - A))
@@ -38,8 +38,8 @@ def Jacobi(A, x, y, epsilon):
             x = _rekke(x_ny)
         return x
 
-    lol = _rekke(x)
-    return lol
+    x_answ = _rekke(x)
+    return x_answ
 
 
-print(Jacobi(A, x, y, epsilon))
+print(jacobi(A, x, y, epsilon))
