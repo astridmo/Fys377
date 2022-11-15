c8c7f150ca476a681d850def247a823271733286
astridmo <astrid.moum@gmail.com>
2022-11-15 20:34:04 +0100

    Make Jacobi function

--- Chapter 6/Jacobi.py
+++ Chapter 6/Jacobi.py
@@ -17,7 +17,29 @@ epsilon = 10**-4
 
 print(M)
 
-for _ in range(10):
+for _ in range(50):
     x = np.matmul(M, x) + np.matmul(Dinv, y)
 
-print(x)
+print('FASIT', x)
+
+A = np.array([[8, 2, 1], [4, 6, 2], [3, 4, 14]])
+x = np.array([0, 0, 0])
+
+
+def Jacobi(A, x, y, epsilon):
+    D = np.diag(np.diag(A))  # Diagonal matrix
+    Dinv = np.linalg.inv(D)  # Inverse of matrix D
+    M = np.matmul(Dinv, (D - A))
+
+    def _rekke(x):
+        """Recursion as long as the error>limit"""
+        x_ny = np.matmul(M, x) + np.matmul(Dinv, y)
+        if (abs(x_ny - x) >= epsilon).all():
+            x = _rekke(x_ny)
+        return x
+
+    lol = _rekke(x)
+    return lol
+
+
+print(Jacobi(A, x, y, epsilon))
